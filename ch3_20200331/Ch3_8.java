d40fe6453a477be5dc2e3152dbd7d75d069b558a
xvpowerg <c911010083@gmail.com>
2020-03-31 22:05:11 +0800

    java20200324
    Ch3_1.java 上次作業
    Ch3_2.java  for 迴圈
    Ch3_3.java  for 迴圈 break
    Ch3_4.java  for 迴圈巢狀
    Ch3_5.java  for while
    Ch3_6  array 
    Ch3_7  array 宣告
    Ch3_8  array 排序
    作業:
     1 宣告一陣列 幫我算出 此陣列的總和
     [5,6,8,1,2]  輸出 22
    2 宣告一陣列 幫找出  此陣列的最大與最小
    [7,9,100,5,3,11]  max:100 min: 3

--- ch3_20200331/Ch3_8.java
+++ ch3_20200331/Ch3_8.java
@@ -5,21 +5,30 @@
  */
 package ch3_20200331;
 import java.util.Random;
+import java.util.Arrays;
 public class Ch3_8 {
     public static void main(String[] args) {
 	//java預設都是由小到大排序
 	//java 用快速排序法O(nlogn)
 	int[] myNumber = new int[200];
+	//1 我有一個長度為200的陣列
+	//2 我希望陣列內的數值是1~30000之間的亂數
 	Random ran = new Random();
 	for (int i = 0;i < myNumber.length;i++){
-	    myNumber[i] = ran.nextInt(30000)+1;
+	    myNumber[i] = ran.nextInt(30000)+1;//1~30000之間亂數
 	}
-	
-	for(int i =0;i<10;i++){
-	  System.out.print(myNumber[i]+" ");
+	//可排序 使用快速排序法
+	Arrays.sort(myNumber);
+	for (int i= 0;i<10;i++){
+	    System.out.print(myNumber[i]+" ");
 	}
-	
-	
+	System.out.println();
+	//Java 是使用2分搜尋法 一定要遞增排序排序才能搜尋 
+	//如果找不到一定是負數
+	int search[] =  {6,8,10,25};
+	int index = Arrays.binarySearch(search, 10);
+	System.out.println(index);
+	index = Arrays.binarySearch(search, 9);
+	System.out.println(index);
     }
-    
 }
